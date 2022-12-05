20987ad694b9484226120288ecbe336e934014ef
Princess <princessnwakaego25@gmail.com>
2022-12-05 19:42:28 +0000

    current gown price

---
+++ gown.sh
@@ -0,0 +1,12 @@
+#This app is built for the inventory of new gown arrivals in the month of December
+#it shows the current price of each specified gown to customers
+echo "welcome to Fashionista showroom"
+echo "this is the new arrivals for the month of December"
+echo "price is the same for a specified gown irrespective of size"
+echo "korean gown 3000"
+echo "english gown 5000"
+echo "japaneese gown 3500"
+echo "vietnam gown 5500"
+echo "thanks for checking on us, what's your order?"
+
+
